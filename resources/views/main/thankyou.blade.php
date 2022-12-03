722bf2d2a7ba74176de1a4059f8f2e9a1a47198d
hamza umar <humar6078@gmail.com>
2022-12-03 16:43:50 +0000

    thank you done

--- resources/views/main/thankyou.blade.php
+++ resources/views/main/thankyou.blade.php
@@ -4,17 +4,50 @@
 <div class="content">
     <div class="container mt-5 px-5">
 
-        <div class="mb-4 pre">
-            <span>Hello {{ Auth::user()->name }} </span>
-        </div>
-
         <div class="row">
-
+            <div class="mb-4 pre">
+                <h1>Thank you </h1>
+            </div>
             <div class="col-md-12">
+                <span>Hello {{ Auth::user()->name }}, </span><br>
+                <div>Your order with a the order ID of {{$order->id}} and payment reference
+                    ({{$order->payment_reference}}) has been accepted and ready for shipment to the address below</div>
 
+                <div>
+                    <table class="table border">
+                        <tbody>
+                            <tr>
+                                <td>Street Address</td>
+                                <td colspan="2">{{$order->street_address}}</td>
+                            </tr>
+                            <tr>
+                                <td>City Town</td>
+                                <td colspan="2">{{$order->city_town}}</td>
+                            </tr>
+                            <tr>
+                                <td>Locality</td>
+                                <td colspan="2">{{$order->locality}}</td>
+                            </tr>
+                            <tr>
+                                <td>Region</td>
+                                <td colspan="2">{{$order->region}}</td>
+                            </tr>
+                            <tr>
+                                <td>Delivery Address</td>
+                                <td colspan="2">{{$order->delivery_address}}</td>
+                            </tr>
+                            <tr>
+                                <td>GPS Address</td>
+                                <td colspan="2">{{$order->gps_address}}</td>
+                            </tr>
+                            <tr>
+                                <td>Date</td>
+                                <td colspan="2">{{$order->updated_at->format('d M Y g:i A')}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-                <h1>Thank you </h1>,
-                {{$order}}
             </div>
 
         </div>
@@ -26,48 +59,3 @@
 <script rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <link rel="stylesheet" href="{{ asset('main/assets/css/checkout.css') }}">
-
-<script src="https://js.paystack.co/v1/inline.js"></script>
-
-<script>
-const paymentForm = document.getElementById('paymentForm');
-paymentForm.addEventListener("submit", payWithPaystack, false);
-
-function payWithPaystack(e) {
-    e.preventDefault();
-
-    let handler = PaystackPop.setup({
-        key: 'pk_test_989007f464df9f461740fc76aacd0149bdbd3e62', // Replace with your public key
-        email: document.getElementById("email-address").value,
-        amount: document.getElementById("amount").value * 100,
-        currency: "GHS",
-
-        onClose: function() {
-            alert('Window closed.');
-        },
-        callback: function(response) {
-            let reference = response.reference;
-            //  console.log(reference);
-            $.ajax({
-                type: "GET",
-                url: "{{URL::to('verify-payment')}}/" + reference,
-                success: function(response) {
-                    console.log(response);
-                    if (response[0].status == true) {
-                        $('form').prepend(`
-                                <h2>${response[0].message}</h2>
-                            `)
-                    } else {
-                        $('form').apend(`
-                                <h2>Payment Failed</h2>
-                                <a href=""><<button type="submit" class="btn btn-info px-3 >Retry</button>/a>
-                            `)
-                    }
-                }
-            });
-        }
-    });
-
-    handler.openIframe();
-}
-</script>
